b7e7ff4c5c0d90c6f0348e9e37195f031327b2ad
Diego Ramos <rdiego26@gmail.com>
2013-12-19 01:14:10 -0200

    Nome do arquivo - Animação idêntica a do conteudo

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ $(function(){
    var $animado = $("#result");
    $(".content-open").on("click", function(e){
        e.preventDefault();
+
        var url = $(this).attr('href');
+       //Coloca o nome do arquivo
+       $("#fileNameSel").html(url.split('/')[1]);
+       $("#fileNameSel").stop(true, true).fadeIn('slow').delay(3000).fadeOut();
+
        $("#result").load(url);
        $animado.stop(true, true).fadeIn('slow').delay(3000).fadeOut();
    });
