87233651c68c8b83a47c40512f3c43e0adc7ac75
tadeumesquita <tadeumesquita@usp.br>
2018-02-08 09:54:20 -0200

    First contrib

---
+++ tadeumesquita.php
@@ -0,0 +1,11 @@
+<?phph
+/*
+*
+*@authr: Tadeu Mesquita <tadeumesquita@gmail.com>
+*
+*My GitHub: https://github.com/tadeumesquita
+*/
+
+echo 'Olá! Estou aprendendo a contribui com projeto abertos.';
+
+?>
